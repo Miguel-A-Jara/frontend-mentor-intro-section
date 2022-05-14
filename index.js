d9f458763f76f6bf98c308b6332ecdd8ff6c552b
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-05-14 18:54:10 -0400

    Finished the mobile version

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const closeBtn = document.querySelector('#close-btn');
 //Getting our elements
 const modal = document.querySelector('.modal-hidden');
 const sidebar = document.querySelector('.sidebar-container-hidden');
+const listOpeners = document.querySelectorAll('.listOpener');
 
 
 //Buttons functionalities
@@ -20,10 +21,25 @@ closeBtn.addEventListener('click', () => {
   sidebar.classList.remove("sidebar-container-shown");
 });
 
-//Elements functionalities
+/*=====Elements functionalities=====*/
 /*  When we click on the modal, we also want to close it  */
 modal.addEventListener('click', () => {
 
   modal.classList.remove('modal-shown');
   sidebar.classList.remove("sidebar-container-shown");
 });
+
+listOpeners.forEach(listOpener => {
+  listOpener.addEventListener('click', (e) => {
+    
+    //Getting the list items that we clicked
+    const list = document.querySelector(`.${e.target.id}-list`);
+
+    //Getting the arrow items that we clicked
+    const arrow = document.querySelector(`.${e.target.id}-arrow`);
+
+    //toggling the classes
+    list.classList.toggle(`${e.target.id}-list-opened`);
+    arrow.classList.toggle('arrow-opened');
+  })
+})
